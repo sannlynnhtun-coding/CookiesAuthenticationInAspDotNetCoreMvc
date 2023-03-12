97965e97cc86b39713176bd335dc92dc089f5cf8
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-03-12 14:44:04 +0630

    Add project files.

---
+++ CookiesAuthenticationInAspDotNetCoreMvc/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CookiesAuthenticationInAspDotNetCoreMvc.Controllers {
+    public class AuthenticationController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public async Task<IActionResult> Login() {
+            var claims = new List<Claim>() {
+                new Claim("UserName",  "SLH")
+        };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+            return Redirect("/Home");
+        }
+
+        public async Task<IActionResult> Logout() {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CookiesAuthenticationInAspDotNetCoreMvc/Services/DevCode.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace CookiesAuthenticationInAspDotNetCoreMvc.Services {
+    public static class DevCode {
+        public static string GetSpecificClaim(this ClaimsIdentity claimsIdentity, string claimType) {
+            var claim = claimsIdentity.Claims.FirstOrDefault(x => x.Type == claimType);
+
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
